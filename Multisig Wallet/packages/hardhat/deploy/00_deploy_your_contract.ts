ed9ad8c7a7e6d589e81420abf57ec562c730c6ff
Bleyle823 <bennybleylle09@gmail.com>
2025-08-01 01:22:37 +0300

    Refined frontend & Backend 💸

--- Multisig Wallet/packages/hardhat/deploy/00_deploy_your_contract.ts
+++ Multisig Wallet/packages/hardhat/deploy/00_deploy_your_contract.ts
@@ -22,10 +22,10 @@ const deployYourContract: DeployFunction = async function (hre: HardhatRuntimeEn
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  await deploy("PiggyBank", {
+  await deploy("YourContract", {
     from: deployer,
     // Contract constructor arguments
-    // args: [deployer], // Removed, PiggyBank has no constructor args
+    args: [deployer],
     log: true,
     // autoMine: can be passed to the deploy function to make the deployment process faster on local networks by
     // automatically mining the contract deployment transaction. There is no effect on live networks.
@@ -33,12 +33,12 @@ const deployYourContract: DeployFunction = async function (hre: HardhatRuntimeEn
   });
 
   // Get the deployed contract to interact with it after deploying.
-  const piggyBank = await hre.ethers.getContract<Contract>("PiggyBank", deployer);
-  console.log("✅ PiggyBank deployed at:", await piggyBank.getAddress());
+  const yourContract = await hre.ethers.getContract<Contract>("YourContract", deployer);
+  console.log("👋 Initial greeting:", await yourContract.greeting());
 };
 
 export default deployYourContract;
 
 // Tags are useful if you have multiple deploy files and only want to run one of them.
 // e.g. yarn deploy --tags YourContract
-deployYourContract.tags = ["PiggyBank"];
+deployYourContract.tags = ["YourContract"];
